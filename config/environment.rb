1a8513371a9a28338510db5334c70135a182a86d
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-11-01 13:22:44 +0300

    completed phase 3 orms mapping database records to ruby objects

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,5 @@ Bundler.require
 
 require_relative '../lib/song'
 
+# Notice that the value of the connection in this hash is actually a new instance of the SQLite3::Database class. This is how we will connect to our database. Our database instance responds to a method called execute that accepts raw SQL as a string. Let's pass in that SQL we stored above
 DB = { conn: SQLite3::Database.new("db/music.db") }
